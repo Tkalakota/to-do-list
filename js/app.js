bf26bb6329fc7c3d4cbc5cf4ab4df29c50a26ac5
Tkalakota <tejak3525@gmail.com>
2016-06-04 11:31:19 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,31 +1,31 @@
-angular.module("todoApp", [])
+angular.module("todoApp", [])      //Start of  funtion
 .controller("TodoListController", function($scope) {
   var todoList = this;
   todoList.todoItems = [
    
-        { name: "-Drop out of school",
+        { name: "-Drop out of school", //given examples
  done: false
  },
-		{ name: "-Follow your dreams",
+    { name: "-Follow your dreams",  //given examples
  done: false
  },
-		{name:"-Become selfish",
+    {name:"-Become selfish",  //given examples
  done: false
 },
-		{ name: "-Make Billions",
+    { name: "-Make Billions",  //given examples
  done: false
  },
-       { name: "Live Your life as a pleasant wealthy object", done: false }
+       { name: "Live Your life as a pleasant wealthy object", done: false } //given examples
   ]
 
-  todoList.addTodo = function() {
+  todoList.addTodo = function() { // adds list items
     todoList.todoItems.push(
       { name: todoList.todoText }
     )
     todoList.todoText = "";
   };
 
-  todoList.remove = function() {
+  todoList.remove = function() {  // removes list items
     var oldTodos = todoList.todoItems;
     todoList.todoItems = [];
     angular.forEach(oldTodos, function(todo) {
@@ -34,7 +34,7 @@ angular.module("todoApp", [])
     todoList.todoItems
   };
 
-  todoList.remaining = function() {
+  todoList.remaining = function() { // leftover on the list
     var notCompletedCount = 0;
     angular.forEach(todoList.todoItems, function(todo) {
       notCompletedCount += todo.done ? 0 : 1;
